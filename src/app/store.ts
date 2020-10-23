43de5b61fd477cab6e79355b16cf25bfc72f5c5a
csabagaspar <gaspar.csaba@gmail.com>
2020-10-23 11:53:24 +0200

    turn of default middlewares

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,15 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
 import questionsReducer from '../features/questions/questionsSlice'
 
 export const store = configureStore({
   reducer: {
     questions: questionsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+	  thunk: false,
+	  immutableCheck: false,
+	  serializableCheck: false
+  })
 })
 
 export type RootState = ReturnType<typeof store.getState>
