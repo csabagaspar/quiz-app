9f2db0c4a2a14b623f7d2386f294e806b541989c
csabagaspar <gaspar.csaba@gmail.com>
2020-10-24 19:51:24 +0200

    Updates

---
+++ precache-manifest.7feb53ca1a0b63974e0d45bebb0d2cd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888fc52603b7ef0a359da88370448613",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "65ed17c1f849afa71847",
+    "url": "/quiz-app/static/js/2.0f9950a0.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/quiz-app/static/js/2.0f9950a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88fc6e3df2bd59d60e8c",
+    "url": "/quiz-app/static/js/main.75f0aa1c.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
